7bf1195cecb786c38d3bf15cc888035dfec3f69b
pedroichimaru <pedroichimaru@gmail.com>
2025-12-07 18:38:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "races.db")
+DB_PATH = os.path.join(BASE_DIR, "var/data/races.db")
 
 
 def init_db():
